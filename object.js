354e07ba5072475f1134ea100664f5a83d493c74
Anum Emmanuel <anumemma24@gmail.com>
2026-01-10 04:47:31 +0000

    first

---
+++ object.js
@@ -0,0 +1,23 @@
+let pupil ={
+    name:'Anum Emmanuel',
+    Email: "anumemma24@gmail.com",
+    age: 70,
+}
+
+console.log(pupil)
+
+let inport = {
+    package: "Shoe",
+    Number: 33567886,
+    location: 'Ashiaman',
+}
+
+console.log(inport)
+
+let package = {
+    Name: "Akosua Mary",
+    call: +23324-364-7409,
+    Amount: 'GH₡ 60',
+    Location: "Osu",
+}
+console.log(package)
