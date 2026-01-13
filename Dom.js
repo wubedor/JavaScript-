55a489d2a445e47afaec0088633801f926c56ea6
Anum Emmanuel <anumemma24@gmail.com>
2026-01-13 03:51:04 +0000

    first

---
+++ Dom.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('heading1').innerHTML='Hello World'
+
+
+let nameChange = document.getElementsByTagName('p')[0]
+//nameChange.innerHTML= 'Changed Text'
+
+let className = document.getElementsByClassName('firstclass')[0]
+
+let image = document.getElementById('image')
+image.src='alan-hardman-SU1LFoeEUkk-unsplash.jpg'
+image.style.height='60px'
+
+nameChange.style.color='blue'
