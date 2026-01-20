d1e49aad219122b96873f9d7da5efdf9ea8d3bbc
Anum Emmanuel <anumemma24@gmail.com>
2026-01-20 13:54:56 +0000

    first

---
+++ Javascript.js
@@ -0,0 +1,39 @@
+//string
+var name= 'Hello'
+var course= "I.T"
+var school= "Code Africa"
+var church= "Modem International"
+var homeTown="Accra"
+
+//integer
+var year= 2024
+var age= 30
+var numberOfsiblings= 4
+var streetNo= 27
+var houseNo= 15
+
+//float
+var height= 5.9
+var weight= 65.5
+let temperature= 36.6
+let humidity= 78.4
+let result= -86.7
+
+//boolean
+let power= true
+let child= false
+let baby= false
+let mother= true
+let father= true
+
+//document.write
+document.write(name)
+document.write(power)
+document.write(year)
+document.write(height)
+document.write(course)
+document.write(school)
+document.write(church)
+document.write(homeTown)
+document.write(age)
+document.write(numberOfsiblings)
